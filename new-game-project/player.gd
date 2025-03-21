25bdfec6681cee3bddc76d2d06bf07649be64129
Kunut0 <maxcunmun@gmail.com>
2025-03-21 21:08:22 +0100

    yay more physics

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -2,20 +2,19 @@ extends CharacterBody2D
 
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+const JUMP_VELOCITY = -500.0
+var MAX_SPEED = 700.0
+var SPEED = 200
 var DoubleJump= 0
 var Dash=0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var anim = get_node("AnimationPlayer")
+#@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	
-	if is_on_wall_only() :
-		velocity.y += gravity * delta  
-		velocity.y = velocity.y *0.8
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -35,7 +34,14 @@ func _physics_process(delta):
 		get_node("Sprite2D").flip_h = false
 	if direction:
 		velocity.x = direction * SPEED
+		if (SPEED < MAX_SPEED):
+			SPEED += 20
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		SPEED = 300
 	move_and_slide()
 	
+	if is_on_wall():
+		SPEED = 300
+	
+	print(velocity.x)
