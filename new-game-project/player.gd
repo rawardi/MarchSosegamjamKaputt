01fcf755b5a58e3553b927b400cbbcecb99333a9
rawardi <160549735+rawardi@users.noreply.github.com>
2025-03-21 20:49:18 +0100

    Wallclutch

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var DoubleJump= 0
-
+var Dash=0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,13 +13,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	
-	if is_on_wall() and Input.is_action_just_pressed("Wallclutch") :
-		velocity.y += gravity * delta * 2
+	if is_on_wall_only() :
+		velocity.y += gravity * delta  
+		velocity.y = velocity.y *0.8
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and DoubleJump <= 2 :
+	if Input.is_action_just_pressed("ui_up") and DoubleJump <= 1 :
 		velocity.y = JUMP_VELOCITY
 		DoubleJump+=1
 	if is_on_floor() :
