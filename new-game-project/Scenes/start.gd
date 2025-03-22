f754ab4eda3033ccb85ccd706f2a3d8e694a642c
rawardi <160549735+rawardi@users.noreply.github.com>
2025-03-22 20:03:13 +0100

    startscenerichtig

--- new-game-project/Scenes/start.gd
+++ new-game-project/Scenes/start.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var leaderboard="res://Scenes/Leaderboardtest.tscn"
 @export var level="res://Scenes/level_1.tscn"
-
+var leaderboardisshown=false
 func _on_start_pressed() -> void:
 	var loadlevel= load(level)
 	get_tree().change_scene_to_packed(loadlevel)
@@ -11,5 +11,9 @@ func _on_start_pressed() -> void:
 
 
 func _on_leaderboard_pressed() -> void:
-	var loadleaderboard= load(leaderboard)
-	get_tree().change_scene_to_packed(loadleaderboard)
+	if leaderboardisshown :
+		$LeaderboardUI.hide()
+		leaderboardisshown=false
+	else :
+		$LeaderboardUI.show()
+		leaderboardisshown=true
