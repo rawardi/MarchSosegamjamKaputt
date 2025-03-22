8be9966fff87bb37305fb82cec9993bb6394a96f
Kunut0 <maxcunmun@gmail.com>
2025-03-22 22:13:41 +0100

    retry

--- new-game-project/Scenes/endscreen.gd
+++ new-game-project/Scenes/endscreen.gd
@@ -10,9 +10,12 @@ func _ready() -> void:
 	playertime *= 1000
 	playertime = int(playertime)
 	playertime /= 1000
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
 	await Leaderboards.post_guest_score("marchsosegamejam2025-leaderboard-G93R", playertime, playername)
-=======
+#=======
+#=======
+#>>>>>>> Stashed changes
 	endtext.text = "[center][font_size=128]WICKED FINISH![/font_size][p][center][font_size=32]Your Time: " + str(playertime) + " seconds"
 	$LineEdit.placeholder_text = "SUBMIT NAME"
 	$LineEdit.grab_focus()
@@ -20,7 +23,7 @@ func _ready() -> void:
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	playername = new_text
 	await Leaderboards.post_guest_score("marchsosegamejam2025-leaderboard-GU3d", playertime, playername)
->>>>>>> Stashed changes
+#>>>>>>> Stashed changes
 	Global.speed_runtime=0
 	$LineEdit.text = ""
 	$LineEdit.queue_free()
