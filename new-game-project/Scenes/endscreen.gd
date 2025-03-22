07b78bb4e959f6e53d473745d8f37753800d4b00
rawardi <160549735+rawardi@users.noreply.github.com>
2025-03-22 21:13:07 +0100

    highscore

--- new-game-project/Scenes/endscreen.gd
+++ new-game-project/Scenes/endscreen.gd
@@ -9,7 +9,7 @@ func _on_line_edit_text_submitted(new_text: String) -> void:
 	playertime *= 1000
 	int(playertime)
 	playertime /= 1000
-	await Leaderboards.post_guest_score("marchsosegamejam2025-leaderboard-GU3d", playertime, playername)
+	await Leaderboards.post_guest_score("marchsosegamejam2025-leaderboard-G93R", playertime, playername)
 	Global.speed_runtime=0
 	get_tree().reload_current_scene()
 	$HBoxContainer/LineEdit.text = ""
